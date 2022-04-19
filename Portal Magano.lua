d3b3628a7ea488f7740e93bc7f153568f1642449
Daniel Tavares <nieltvrs@gmail.com>
2022-04-19 16:35:13 -0300

    Update Portal Magano.lua

--- Portal Magano.lua
+++ Portal Magano.lua
@@ -3,25 +3,23 @@ function onUse(cid, item)
 local TeleportID = 1387
 local Teleport = {
 config = {
-ToPos = {x = 157, y = 51, z = 7},
 Pos = getCreaturePosition(cid),
+ToPos = {x = getCreaturePosition(cid).x, y = getCreaturePosition(cid).y, z = getCreaturePosition(cid).z - 3},
 Time = 10,
 },
 }
 
 function removeTeleport(Teleport)
-	local t = getTileItemById(Teleport.config.Pos, TeleportID)
-	if t then
-		doRemoveItem(t.uid, 1)
-		doSendMagicEffect(Teleport.config.Pos, CONST_ME_POFF)
-	end
+    local t = getTileItemById(Teleport.config.Pos, TeleportID)
+    if t then
+        doRemoveItem(t.uid, 1)
+        doSendMagicEffect(Teleport.config.Pos, CONST_ME_POFF)
+    end    
 end
 
-	doCreateTeleport(TeleportID, Teleport.config.ToPos,Teleport.config.Pos)
-	doCreatureSay(cid, "Kamigami no potaru (Portal dos Deuses) ", TALKTYPE_ORANGE_1) 
-	addEvent(removeTeleport, Teleport.config.Time * 1000, Teleport)
-	doRemoveItem(item.uid,1)
-	return TRUE
+    doCreateTeleport(TeleportID, Teleport.config.ToPos,Teleport.config.Pos)
+    doCreatureSay(cid, "Kamigami no potaru (Portal dos Deuses) ", TALKTYPE_ORANGE_1)
+    addEvent(removeTeleport, Teleport.config.Time * 1000, Teleport)
+    doRemoveItem(item.uid,1)
+    return TRUE
 end
-
-<action itemid="x" event="script" value="portal.lua"/>
